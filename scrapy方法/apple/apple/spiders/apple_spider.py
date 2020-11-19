a54c7b36c37635e365e42675c9625b8ae2ac7579
crcleavity <50039055+crcleavity@users.noreply.github.com>
2020-11-20 00:33:12 +0800

    scrapy方法

---
+++ scrapy方法/apple/apple/spiders/apple_spider.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from apple.items import AppleItem
+import scrapy
+
+
+class AppleSpiderSpider(scrapy.Spider):
+    name = 'apple_spider'
+    allowed_domains = ['www.apple.com.cn']
+    start_urls = ['https://www.apple.com.cn/retail/storelist/']
+
+    def parse(self, response):
+        shop_list = response.xpath('//*[@id="cnstores"]/div/div/div/div/ul')
+        for shop in shop_list:
+            apple = AppleItem()
+            apple['shop_name'] = shop.xpath('li[1]/a/text()').get()
+            apple['address'] = shop.xpath('li[2]/text()').get()
+            apple['contact'] = shop.re('400-\d{3}-\d{4}')
+            apple['href'] = shop.xpath('li[1]/a/@href').get()
+            
+            yield apple
